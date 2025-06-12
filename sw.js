9856e7316b8fabf6d309a91446afbbc26f3011e3
proudfish07 <34559512+proudfish07@users.noreply.github.com>
2025-06-12 09:10:08 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'wildlife-triangulation-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  // 加上你有的其它資源（如圖片、icon、logo等）
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      )
+    )
+  );
+});
